fc17c19f6d730150875d095a928bdf04e711c59a
moralmunky <christopher@moralmunky.com>
2022-01-02 21:35:03 -0500

    Update 3

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM selenium/standalone-chrome-debug
 
 USER root
+
 RUN apt-get update && apt-get upgrade -y && apt-get install -y \
 	build-essential python-dev python-setuptools python3-pip python3-smbus \
 	libncursesw5-dev libgdbm-dev libc6-dev \
--- python/smartrent-bridge.py
+++ python/smartrent-bridge.py
@@ -53,8 +53,8 @@ mqtt_client.username_pw_set(MQTT_USER, password=MQTT_PASS)
 #     mqtt_client.tls_set(cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 #     mqtt_client.tls_insecure_set(not MQTT_TLS)
 mqtt_client.on_connect = on_mqtt_connect
-print(devices)
-print(mqtt_client.__dict__)
+# print(devices)
+# print(mqtt_client.__dict__)
 
 class SmartRentBridge:
     ws_message = ''
