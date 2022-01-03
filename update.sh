41eca395432ec625b2badfd295fab60a1bd05ef4
moralmunky <christopher@moralmunky.com>
2022-01-02 20:36:17 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 cd ..
-rm master.zip
+rm main.zip
 wget "https://github.com/moralmunky/SmartRent-MQTT-Bridge-Robert-Friedland/archive/refs/heads/main.zip"
 unzip -o main.zip
-cd SmartRent-MQTT-Bridge-master-Robert-Friedland
+cd SmartRent-MQTT-Bridge-Robert-Friedland-main
 docker build . -t smartrent-mqtt-bridge
