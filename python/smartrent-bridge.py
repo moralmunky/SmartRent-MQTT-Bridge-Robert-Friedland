052c66590239d236d8ec831f9afe8bf888171928
moralmunky <christopher@moralmunky.com>
2022-01-02 21:01:30 -0500

    Update smartrent-bridge.py

--- python/smartrent-bridge.py
+++ python/smartrent-bridge.py
@@ -36,7 +36,7 @@ ws_message = ''
 
 #def on_mqtt_connect(self, client, userdata, flags, rc):
 def on_mqtt_connect(self, client, userdata, flags, rc=None):
-    print("Connected to MQTT broker with result code " + str(rc))
+    print("Connected to MQTT broker with result code " + str(self) + str(client) + str(userdata) + str(flags) + str(rc))
 
 mqtt_client = mqtt.Client(transport="websockets")
 mqtt_client.username_pw_set(MQTT_USER, password=MQTT_PASS)
